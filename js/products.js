d06e1071c09f925c5f88c4303aeabe3d75b07f40
Oliver Skanberg-Tippen <oliverskanbergtippen@gmail.com>
2018-01-07 13:26:43 +0000

    automatically populate the products page

---
+++ js/products.js
@@ -0,0 +1,41 @@
+const endpoint = "http://178.62.24.41:3003/api/collections/get/products?token=a9bed48fd1e3ba6f4f37ec8a88aaca";
+
+
+const template = document.getElementById("template").children[0];
+function newProduct(name, description, price, image) {
+    let node = template.cloneNode(true);
+    // name
+    node.getElementsByClassName("product-name")[0].innerHTML = name;
+
+    // description
+    node.getElementsByClassName("product-description")[0].innerHTML = description;
+
+    // price
+    node.getElementsByClassName("product-price")[0].innerHTML = price;
+
+    // image
+    node.getElementsByClassName("product-image")[0].src = image;
+    return node;
+}
+
+const productSection = document.getElementById("products");
+function addEntry(product) {
+    let p = newProduct(product.Name, product.Description, product.Price, product.Image.path)
+    productSection.appendChild(p);
+}
+
+function loadProducts() {
+    fetch(endpoint)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (products) {
+            products.entries.forEach(addEntry)
+        })
+        .catch(function (error) {
+            console.error("oh no!");
+            console.error(error);
+        });
+}
+
+loadProducts();
